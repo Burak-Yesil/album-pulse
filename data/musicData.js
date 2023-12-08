62f82a435432553949797a6f2dea937733b574d1
sanya mann <sanyamann03@gmail.com>
2023-12-07 20:26:50 -0500

    setup headers

---
+++ data/musicData.js
@@ -0,0 +1,20 @@
+import * as spotify from './spotifyAPI.js'
+
+
+export const albumInfo = async (name) => {
+// adds album to database with info from spotify API and average ranking
+
+}
+
+export const getRankings = async (name) => {
+// checks if album has been ranked yet. If yes, returns rankings and information for that album. 
+// If not, finds album from spotifyAPI and returns that there are no rankings
+
+
+}
+
+export const addRanking = async (rating, review) => {
+// lets user add ranking and then adds ranking to the rankings database
+// also adds ranking to the array of rankings in the album object; edits albums database 
+
+}
